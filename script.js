7eea6464049d2302683ec4b24177e77eb0b02e89
ethank2222 <59261562+ethank2222@users.noreply.github.com>
2020-04-20 14:43:57 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+function page(){
+	window.location.replace('https://www.google.com')
+}
+function darkbutton(){
+	document.getElementById('rect1').style.setProperty("fill","#275d8b","important")
+}
+function darkbuttonoff(){
+	document.getElementById('rect1').style.setProperty("fill","#428bca")
+}
+function darkbutton2(){
+	document.getElementById('rect2').style.setProperty("fill","#275d8b","important")
+}
+function darkbuttonoff2(){
+	document.getElementById('rect2').style.setProperty("fill","#428bca")
+}
+function darkbutton3(){
+	document.getElementById('rect3').style.setProperty("fill","#275d8b","important")
+}
+function darkbuttonoff3(){
+	document.getElementById('rect3').style.setProperty("fill","#428bca")
+}
+
+function gototop(){
+	window.scrollTo(0,0)
+}
